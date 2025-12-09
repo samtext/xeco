f2ac20a70dfa55e077c19d148df58025538b97e8
SAMTEXT <samtext454@gmail.com>
2025-12-09 17:53:43 +0300

    Updated .env and database configuration for PostgreSQL

--- config/database.php
+++ config/database.php
@@ -63,18 +63,20 @@ return [
         ],
 
         'pgsql' => [
-            'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),  // This will come from .env (Replace with Render host if necessary)
-            'port' => env('DB_PORT', '5432'),  // PostgreSQL default port
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'search_path' => 'public',
-            'sslmode' => 'prefer',  // For production PostgreSQL (can be adjusted if needed)
+    'driver' => 'pgsql',
+    'url' => env('DATABASE_URL'),
+    'host' => env('DB_HOST', '127.0.0.1'),
+    'port' => env('DB_PORT', '5432'),
+    'database' => env('DB_DATABASE', 'forge'),
+    'username' => env('DB_USERNAME', 'forge'),
+    'password' => env('DB_PASSWORD', ''),
+    'charset' => 'utf8',
+    'prefix' => '',
+    'prefix_indexes' => true,
+    'search_path' => 'public',
+    'sslmode' => 'prefer',  // You might want to use SSL mode
+
+
         ],
 
         'sqlsrv' => [
